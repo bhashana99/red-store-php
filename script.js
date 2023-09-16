bdff3d881f28ddfd3fa4f2355b8a684039b25284
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2023-09-16 22:43:15 +0530

    add product-details page

--- script.js
+++ script.js
@@ -30,3 +30,20 @@ function menutoggle(){
         menuItem.style.maxHeight = "0px";
     }
 }
+
+// product gallery
+const productImg = document.getElementById("product-img");
+let smallImg = document.getElementsByClassName("small-img");
+
+smallImg[0].onclick = function(){
+    productImg.src = smallImg[0].src;
+}
+smallImg[1].onclick = function(){
+    productImg.src = smallImg[1].src;
+}
+smallImg[2].onclick = function(){
+    productImg.src = smallImg[2].src;
+}
+smallImg[3].onclick = function(){
+    productImg.src = smallImg[3].src;
+}
