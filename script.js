95e2520e365a10d1be15685e2ab41af7e3a03ff0
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2023-09-16 17:18:40 +0530

    add media query

--- script.js
+++ script.js
@@ -18,3 +18,15 @@ mode.onclick = function(){
         }
     }
 }
+
+const menuItem = document.getElementById("menuItem");
+
+menuItem.style.maxHeight = "0px";
+
+function menutoggle(){
+    if(menuItem.style.maxHeight == "0px"){
+        menuItem.style.maxHeight = "200px";
+    }else{
+        menuItem.style.maxHeight = "0px";
+    }
+}
