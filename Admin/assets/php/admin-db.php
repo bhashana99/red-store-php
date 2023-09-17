30f21b2304f7b4ca07b41f33f2765a64b2f43f93
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2023-09-17 18:50:33 +0530

    add admin login function

--- Admin/assets/php/admin-db.php
+++ Admin/assets/php/admin-db.php
@@ -3,7 +3,15 @@
 require_once './config.php';
 
 class Admin extends Database{
-    
+    //Admin Login
+    public function admin_login($username,$password){
+        $sql = "SELECT username, password FROM admin WHERE username = :username AND password=:password ";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute(['username'=>$username, 'password'=>$password]);
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        return $row;
+    }
 }
 
 
