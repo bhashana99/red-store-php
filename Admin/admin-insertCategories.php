afa2e528bc977a0dca64c85acf5c4353ec805b0f
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2023-09-18 12:20:51 +0530

    design add category row

--- Admin/admin-insertCategories.php
+++ Admin/admin-insertCategories.php
@@ -4,6 +4,26 @@ require_once './assets/php/admin-header.php';
 
 ?>
 
+<div class="container mt-5">
+    <div class="row">
+        <div class="col-md-6">
+            <h2>Add New Categories</h2>
+
+            <form action="">
+    <div class="form-row align-items-center">
+        <div class="col">
+            <input type="text" class="form-control" id="newCategoryName" placeholder="Enter category name" autofocus>
+        </div>
+        <div class="col-auto">
+            <input type="submit" class="btn btn-success" value="Submit">
+        </div>
+    </div>
+</form>
+
+        </div>
+    </div>
+    
+</div>
 
 
 
