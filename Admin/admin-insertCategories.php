764f130f28e226cc2b06a4969a9c7baaf286fb0c
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2023-09-18 13:12:05 +0530

    create add new category ajax request

--- Admin/admin-insertCategories.php
+++ Admin/admin-insertCategories.php
@@ -10,13 +10,13 @@ require_once './assets/php/admin-db.php';
         <div class="col-md-6">
             <h2>Add New Categories</h2>
 
-            <form action="">
+            <form action="#" id="categoryAddForm" class="px-3">
     <div class="form-row align-items-center">
         <div class="col">
             <input type="text" class="form-control" id="newCategoryName" placeholder="Enter category name" autofocus>
         </div>
         <div class="col-auto">
-            <input type="submit" class="btn btn-success" value="Submit">
+            <input type="submit" class="btn btn-success" id="categoryAddBtn" value="Submit">
         </div>
     </div>
 </form>
@@ -35,5 +35,28 @@ require_once './assets/php/admin-db.php';
         </div>
 </div>
     
+
+
+<script>
+    $(document).ready(function(){
+        //Add new Category Ajax request
+        $("#categoryAddBtn").click(function(e){
+            if($("#categoryAddForm")[0].checkValidity()){
+                e.preventDefault();
+
+                $(this).val('Please Wait..');
+                $.ajax({
+                    url:'assets/php/admin-action.php',
+                    method:'post',
+                    data:$("#categoryAddForm")+'&action=categoryAdd',
+                    success:function(response){
+                        console.log(response);
+                    }
+                })
+            }
+        });
+
+    });
+</script>
 </body>
 </html>
--- Admin/assets/php/admin-db.php
+++ Admin/assets/php/admin-db.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once './config.php';
+require_once 'config.php';
 
 class Admin extends Database{
     //Admin Login
