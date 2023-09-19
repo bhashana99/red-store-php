bc0e0dbad2540a43ddfb0406878ab78bb675bbd2
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2023-09-19 08:48:17 +0530

    start working on insert product page

--- Admin/admin-insertProduct.php
+++ Admin/admin-insertProduct.php
@@ -4,6 +4,32 @@ require_once './assets/php/admin-header.php';
 
 ?>
 
+<div class="container mt-5">
+    <div class="row">
+        <div class="col-md-6 ">
+            <h2>Add New Product</h2>
+
+            <form action="#" id="productAddForm" class="px-3 ">
+                <div id="productAddError"></div>
+                <div class="form-row align-items-center">
+                    <div class="form-row mt-2">
+                        <label for="product_title" class="form-label">Product Title</label>
+                        <input type="text" class="form-control" id="product_title" placeholder="Enter product title" autofocus required autocomplete="off">
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+
+
+
+<!-- footer area -->
             </div>
         </div>
 </div>
